7d8a30825b7aa0182d81d437311da500648fc41c
Reinholds Zviedris <reinholds@zviedris.lv>
2020-03-24 16:39:18 +0200

    OPS-0 fixed errors in module

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 data "aws_vpc" "selected" {
   count = var.vpc_name != "" ? 1 : 0
-  tags {
+  tags = {
     Name = var.vpc_name
   }
 }
 
 resource "aws_route53_zone" "this" {
   name = var.domain_name
-  vpc = {
+  vpc {
     vpc_id = var.vpc_name != "" ? data.aws_vpc.selected.id : null
   }
   tags    = var.tags
